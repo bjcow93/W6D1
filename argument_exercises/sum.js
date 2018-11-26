1a3aa2bf121c1ec4e138984feecfe73a3c16d8b9
bjcow93 <benjcowden@gmail.com>
2018-11-26 15:15:07 -0800

    Complete prototype.curry with apply

--- argument_exercises/sum.js
+++ argument_exercises/sum.js
@@ -36,20 +36,36 @@
 
 //curriedSum
 
-function curriedSum(numArgs) {
-  const nums = [];
+// function curriedSum(numArgs) {
+//   const nums = [];
+//
+//   const _curriedSum = function (num) {
+//     nums.push(num);
+//     if (nums.length === numArgs){
+//       let sum = 0;
+//       for (let i = 0; i < nums.length; i++) {
+//         sum += nums[i];
+//       }
+//         return sum;
+//     } else {
+//       return _curriedSum;
+//     }
+//   };
+//   return _curriedSum;
+// }
 
-  const _curriedSum = function (num) {
-    nums.push(num);
-    if (nums.length === numArgs){
-      let sum = 0;
-      for (let i = 0; i < nums.length; i++) {
-        sum += nums[i];
-      }
-        return sum;
+Function.prototype.curry = function(numArgs) {
+  let nums = [];
+  let func = this;
+
+  const _curriedSum = function () {
+    let args = Array.from(arguments);
+    nums = nums.concat(args);
+    if (nums.length >= numArgs){
+      return func.apply(func, nums);
     } else {
       return _curriedSum;
     }
   };
   return _curriedSum;
-}
+};
