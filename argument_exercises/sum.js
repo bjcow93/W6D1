6b9102470e4545d4e2701b663623ac0c462a64d6
bjcow93 <benjcowden@gmail.com>
2018-11-26 14:47:59 -0800

    Write sum, myBind, and curriedSum functions

---
+++ argument_exercises/sum.js
@@ -0,0 +1,55 @@
+// function sum() {
+//   let array = Array.from(arguments);
+//   let sum = 0;
+//
+//   array.forEach((num) => sum += num);
+//   return  sum;
+// }
+//
+// function sum2(...args) {
+//   let sum = 0;
+//
+//   args.forEach((num) => sum += num);
+//   return  sum;
+// }
+//
+//
+//
+//
+// Function.prototype.myBind = function () {
+//   let array = Array.from(arguments);
+//   const func = this;
+//
+//   return function() {
+//     let args = Array.from(arguments);
+//     return func.apply(array[0], array.slice(1).concat(args));
+//   };
+// };
+//
+//
+// Function.prototype.myBind2 = function (ctx, ...bindArgs) {
+//   return (...callArgs) => {
+//     return this.apply(ctx, bindArgs.concat(callArgs));
+//   };
+// };
+
+
+//curriedSum
+
+function curriedSum(numArgs) {
+  const nums = [];
+
+  const _curriedSum = function (num) {
+    nums.push(num);
+    if (nums.length === numArgs){
+      let sum = 0;
+      for (let i = 0; i < nums.length; i++) {
+        sum += nums[i];
+      }
+        return sum;
+    } else {
+      return _curriedSum;
+    }
+  };
+  return _curriedSum;
+}
